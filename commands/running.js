caed9bb2b780069ff0b3b923f4b25cb3ba513c57
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-01-10 16:21:50 +0800

    Update running.js

--- commands/running.js
+++ commands/running.js
@@ -4,7 +4,7 @@ const config = require('../config.js');
 module.exports = {
     async command(client) {
         client.on('messageCreate', async (message) => {
-            if (message.author.id === client.user.id && message.content.toLowerCase() === 'config.cmdrun) {
+            if (message.author.id === client.user.id && message.content.toLowerCase() === config.cmdrun) {
                 config.channelId = message.channel.id;
                 interval.running(client);
             }
