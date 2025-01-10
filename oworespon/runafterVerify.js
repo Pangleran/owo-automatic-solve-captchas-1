ab4dd425e283969a52fe15c4ccc1a0283071b612
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-01-10 16:33:35 +0800

    Update and rename verify.js to runafterVerify.js

--- oworespon/runafterVerify.js
+++ oworespon/runafterVerify.js
@@ -5,7 +5,7 @@ module.exports = {
     async respon(client) {
         client.on('messageCreate', async (message) => {
             if (message.author.id === config.owoId && message.channel.type === 'DM' && message.content.includes('Thank')) {
-                setTimeout(() => interval.running(client), Math.floor(Math.random() * (15000 - 13000 + 1) + 15000));
+                setTimeout(() => return interval.running(client), Math.floor(Math.random() * (15000 - 13000 + 1) + 15000));
             }
         });
     }
