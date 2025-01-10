e866ef30f026b6735a479c48e1f5b5d645e71595
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-01-10 16:15:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
     
     channelId: '-',
     
-    cookie: 'connect.sid=s%3A1p9RF_Z4eYW-ayPAGsGRc0IcJyrYKj3v.8w%2BViQf5bgfBUX6TkV5UL6701824qF9Jn27UWneZp1A'
+    cookie: 'connect.sid=xxxxx'
 }
