00222e282ac0ec86eabf370da15873ab4f51961c
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-10-26 12:48:53 +0800

    Update and rename runafterVerify.js to startfarm-owoverify.js

--- oworespon/startfarm-owoverify.js
+++ oworespon/startfarm-owoverify.js
@@ -1,11 +1,11 @@
-const interval = require('../interval.js');
+const { running } = require('../interval.js');
 const config = require('../config.js');
 
 module.exports = {
     async respon(client) {
         client.on('messageCreate', async (message) => {
             if (message.author.id === config.owoId && message.channel.type === 'DM' && message.content.includes('Thank')) {
-                setTimeout(() => return interval.running(client), Math.floor(Math.random() * (15000 - 13000 + 1) + 15000));
+                setTimeout(() => return running(client), Math.floor(Math.random() * (15000 - 13000 + 1) + 15000));
             }
         });
     }
