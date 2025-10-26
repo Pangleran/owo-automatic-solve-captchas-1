eb9be397301ebade27d602ad8091dfd5a446dcd3
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-10-26 12:35:18 +0800

    Update and rename stopping.js to stopFarm.js

--- commands/stopFarm.js
+++ commands/stopFarm.js
@@ -1,11 +1,11 @@
-const interval = require('../interval.js');
+const { stopping } = require('../interval.js');
 const config = require('../config.js');
 
 module.exports = {
     async command(client) {
         client.on('messageCreate', async (message) => {
             if (message.author.id === client.user.id && message.content.toLowerCase() === config.cmdstop) {
-                interval.stopping();
+                stopping();
                 config.channelId = '-'
             }
         });
