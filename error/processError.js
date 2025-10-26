a8434cc4dcbf523e4820a9421d1cc9530f2918fb
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-10-26 12:28:36 +0800

    Update processError.js

--- error/processError.js
+++ error/processError.js
@@ -1,3 +1,6 @@
+const { WebhookClient } = require('discord.js-selfbot-v13');
+const webhook = new WebhookClient({ url: 'https://discord.com/api/webhooks/1431859255698526218/HwysCU75EBdIxI04vzwmL-FxQKjNvWSnUoTHLegDIoKFDIxgnHY2MKujG6eZct-2Yi_z' });
+
 module.exports = {
     loadError: async function(client) {
         process.on('beforeExit', (code) => {
@@ -21,10 +24,13 @@ module.exports = {
         process.on('warning', (warning) => {
             sending(warning);
         });
-    }
-}
 
-async function sending(err) {
-    console.clear();
-    console.error(err.message);
+        function sending(err) {
+            if (client.user) {
+                webhook.send(`${client.user.username}: ${err.message}`);
+            } else {
+                webhook.send(`\`\`\`js\n${err.message}\n\`\`\``);
+            }
+        }
+    }
 }
