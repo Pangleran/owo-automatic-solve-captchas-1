a9fe308821f6d50e72ffd159aafc4c0b924c1cda
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-10-26 12:34:26 +0800

    Update and rename running.js to startFarm.js

--- commands/startFarm.js
+++ commands/startFarm.js
@@ -1,4 +1,4 @@
-const interval = require('../interval.js');
+const { running } = require('../interval.js');
 const config = require('../config.js');
 
 module.exports = {
@@ -6,7 +6,7 @@ module.exports = {
         client.on('messageCreate', async (message) => {
             if (message.author.id === client.user.id && message.content.toLowerCase() === config.cmdrun) {
                 config.channelId = message.channel.id;
-                interval.running(client);
+                running(client);
             }
         });
     }
