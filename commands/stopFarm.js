0176c7bcb31c7bdf884d842ab12613f47693acf7
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-10-28 09:49:57 +0800

    Update stopFarm.js

--- commands/stopFarm.js
+++ commands/stopFarm.js
@@ -5,8 +5,12 @@ module.exports = {
     async command(client) {
         client.on('messageCreate', async (message) => {
             if (message.author.id === client.user.id && message.content.toLowerCase() === config.cmdstop) {
-                stopping();
-                config.channelId = '-'
+                cfg.channelId = null;
+                cfg.status = false;
+                await Promise.all([
+                    saveConfig(cfg),
+                    stopping();
+                ]);
             }
         });
     }
