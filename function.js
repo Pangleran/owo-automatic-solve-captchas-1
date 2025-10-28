3f09366d49faf20ea767aab8a85da6031b61ea3a
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-10-28 09:44:11 +0800

    Update function.js

--- function.js
+++ function.js
@@ -15,8 +15,6 @@ function saveConfig(obj) {
 
 const sleep = (min, max) => return new Promise(res => setTimeout(res, Math.floor(Math.random() * (max - min + 1) + max)));
 
-let status = false;
-
 async function hunt(client) {
     const cfg = loadConfig();
     
@@ -25,11 +23,11 @@ async function hunt(client) {
 
     try {
         await channel.sendTyping().catch(() => await new Promise(res => setTimeout(res, 2000)));
-        if (status) await Promise.all([channel.send('owoh'), channel.send('owob')]);
+        if (cfg.status) await Promise.all([channel.send('owoh'), channel.send('owob')]);
         
         await sleep(15000, 16000);
         
-        if (status) hunt(client);
+        if (cfg.status) hunt(client);
     } catch (err) {
         console.error(err.message);
     }
@@ -43,25 +41,32 @@ async function owo(client) {
 
     try {
         await channel.sendTyping().catch(() => await new Promise(res => setTimeout(res, 2000)));
-        if (status) await channel.send('owo');
+        if (cfg.status) await channel.send('owo');
 
         await sleep(10000, 11000);
         
-        if (status) owo(client);
+        if (cfg.status) owo(client);
     } catch (err) {
         console.error(err.message);
     }
 }
 
 async function running(client) {
-    if (status) return;
-    status = true;
+    const cfg = loadConfig();
+    if (cfg.status) return;
+    cfg.status = true;
 
-    await Promise.all([hunt(client), owo(client)]);
+    await Promise.all([
+        saveConfig(cfg),
+        hunt(client),
+        owo(client)
+    ]);
 }
 
 async function stopping() {
-    status = false;
+    const cfg = loadConfig();
+    cfg.status = false;
+    saveConfig(cfg);
 }
 
 module.exports = {
