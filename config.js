af236dc593a2d6c65cab3a5575301b132a572a0a
La Tahzan <133788550+Pangleran@users.noreply.github.com>
2025-10-29 09:23:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 module.exports = {
-    owoId: '408785106942164992',
-    
-    channelId: null,
-
-    cmdRun: 'wrun',
-
-    cmdStop: 'ws',
-
-    status: false,
-    
-    cookie: null
+    "token": "token",
+    "owoId": "408785106942164992",
+    "channelId": null,
+    "cmdRun": "wrun",
+    "cmdStop": "ws",
+    "status": false,
+    "cookie": null
 }
